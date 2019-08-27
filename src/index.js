a94e9bd74d1a56e365f2f3d6f472f03861fb1ac3
unknown <oleg.filatov@gs-labs>
2019-08-27 22:55:24 +0300

    swapi initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/app';
+import SwapiService from './service/swapi-service.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>, 
+    document.getElementById('root'));
+
+
+
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+    people.forEach(man => {
+        console.log(man.name);
+    });
+});
+
+swapi.getPerson(4).then((person) =>{
+    console.log(person.name)
+});
+
+
